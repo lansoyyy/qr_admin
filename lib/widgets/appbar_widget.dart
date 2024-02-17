2390a2b9172ffc8127a881cb3db950d78463687c
Lans12345 <olanalans12345@gmail.com>
2024-02-17 10:17:23 +0800

    removed login

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:metro_admin/screens/auth/login_page.dart';
 import 'package:metro_admin/utils/colors.dart';
 import 'package:metro_admin/widgets/text_widget.dart';
 
@@ -18,50 +17,6 @@ PreferredSizeWidget customAppbar(page, page1, context) {
               text: 'Amusement Park Admin Panel',
               fontSize: 16,
               color: Colors.white)),
-      const SizedBox(
-        width: 10,
-      ),
-      IconButton(
-          onPressed: (() {
-            showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                      title: const Text(
-                        'Logout Confirmation',
-                        style: TextStyle(
-                            fontFamily: 'QBold', fontWeight: FontWeight.bold),
-                      ),
-                      content: const Text(
-                        'Are you sure you want to Logout?',
-                        style: TextStyle(fontFamily: 'QRegular'),
-                      ),
-                      actions: <Widget>[
-                        MaterialButton(
-                          onPressed: () => Navigator.of(context).pop(true),
-                          child: const Text(
-                            'Close',
-                            style: TextStyle(
-                                fontFamily: 'QRegular',
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        MaterialButton(
-                          onPressed: () async {
-                            Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                    builder: (context) => LoginPage()));
-                          },
-                          child: const Text(
-                            'Continue',
-                            style: TextStyle(
-                                fontFamily: 'QRegular',
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ],
-                    ));
-          }),
-          icon: const Icon(Icons.logout)),
       const SizedBox(
         width: 20,
       ),
